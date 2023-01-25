9542c43fb7b5e3c2541af3981293bfd501e89cab
McKayla Lankau <lankaukk@yahoo.com>
2023-01-24 22:55:04 -0500

    deleted unused vars

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../App.css";
-import { useState, useEffect } from "react";
 
 export var Contact = (props) => {
   return <div className="contact" style={{fontSize: 200, color: 'lime'}}>hmu</div>;
