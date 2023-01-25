ce326a8d0c8d38250779ff4dded5f8c757c57555
McKayla Lankau <lankaukk@yahoo.com>
2023-01-24 22:59:22 -0500

    funny contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,5 +2,19 @@ import React from "react";
 import "../App.css";
 
 export var Contact = (props) => {
-  return <div className="contact" style={{fontSize: 200, color: 'lime'}}>hmu</div>;
+  return (
+    <div
+      className="contact"
+      style={{
+        fontSize: 200,
+        color: "lime",
+        display: "flex",
+        width: "100vw",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      hmu
+    </div>
+  );
 };
