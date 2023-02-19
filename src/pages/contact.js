d02df7862b3ef4cbcfab70fdcc72f44e4e5d8196
McKayla Lankau <lankaukk@yahoo.com>
2023-02-18 19:10:13 -0500

    stubbed out contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,47 @@
 import React from "react";
 import "../App.css";
+import "./contact.css";
 
 export var Contact = (props) => {
   return (
     <div
       className="contact"
       style={{
-        fontSize: 200,
-        color: "lime",
         display: "flex",
         width: "100vw",
         alignItems: "center",
         justifyContent: "center",
+        flexDirection: "column",
+        gap: 60,
       }}
     >
-      hmu
+      <div className="talk-to-me">Talk To Me!</div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "60vw",
+          gap: 60,
+        }}
+      >
+        <div className="pro-pic" />
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            flexDirection: "column",
+            gap: 60,
+          }}
+        >
+          <div className="contact-description">
+            My name is Mariel Quadrino, and I’m a communication
+            designer based in Brooklyn, NY. For any project inquiries feel free to email me or DM me through the links below.
+          </div>
+          <div>socials</div>
+        </div>
+      </div>
     </div>
   );
 };
