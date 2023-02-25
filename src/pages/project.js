8572c3afb5aed36870338451554cc406518d5154
McKayla Lankau <lankaukk@yahoo.com>
2023-02-25 15:28:04 -0500

    project page mobile responsive

--- src/pages/project.js
+++ src/pages/project.js
@@ -10,23 +10,15 @@ export var Project = (props) => {
         <div className="project-description">{props.project.description}</div>
       </div>
       <div className="project-images">
-        {/* <div
-          className="project-main-image"
-          style={{
-            backgroundImage: `url(${props.project.images[0]}`,
-          }}
-        /> */}
-        <div className="project-other-images">
-          {props.project.images.map((image) => (
-            <div
-              className="project-image"
-              key={image}
-              style={{
-                backgroundImage: `url(${image}`,
-              }}
-            ></div>
-          ))}
-        </div>
+        {props.project.images.map((image) => (
+          <div
+            className="project-image"
+            key={image}
+            style={{
+              backgroundImage: `url(${image}`,
+            }}
+          ></div>
+        ))}
       </div>
     </div>
   );
