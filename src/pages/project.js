a5dc13bdf2a7d45d82ba57058ddfecfa1a4497a3
McKayla Lankau <lankaukk@yahoo.com>
2023-02-27 19:05:26 -0500

    added videos to projects

--- src/pages/project.js
+++ src/pages/project.js
@@ -9,6 +9,20 @@ export var Project = (props) => {
         <div className="project-title">{props.project.title}</div>
         <div className="project-description">{props.project.description}</div>
       </div>
+      {props.project.video ? (
+        <iframe
+          src={props.project.video}
+          // src='https://www.youtube.com/embed/gBpeTexCQA0'
+          alt="Portfolio Video"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+          className="project-description"
+        ></iframe>
+      ) : (
+        console.log(props.project.video)
+      )}
       <div className="project-images">
         {props.project.images.map((image) => (
           <img
